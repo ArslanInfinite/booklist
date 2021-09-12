a5d04e6e23f9f3a0639d5fa456c85c0a3b46bbf1
Arslan <ali.arslan319@gmail.com>
2021-09-12 14:44:06 -0400

    more updates to UI appending

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ function Book(title, author, isbn){
 
 function UI(){}
 
+// adding method to UI function to add book to list
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list')
+// creating table row element to put new books onto in the UI
+    const row = document.createElement('tr')
+// inserting columns into the table
+row.innerHTML = `
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td>${book.isbn}</td>
+<td><a href="#" class=delete>X<a></td>
+`
+list.appendChild(row)
+}
+
 // Event listeners 
 
 // grabbing book-form id and listening for a submit event
@@ -23,5 +38,11 @@ const book = new Book(title, author, isbn)
 // instantiating UI display for book 
 const ui = new UI()
 ui.addBookToList(book)
+// clearing fields after submit
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = ''
+    document.getElementById('author').value = ''
+    document.getElementById('isbn').value = ''
+}
 event.preventDefault()
 });
