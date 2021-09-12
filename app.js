08c3d345033e92ca4aaded493bfb827c65c8bb75
Arslan <ali.arslan319@gmail.com>
2021-09-11 23:58:06 -0400

    create Book constructor function

---
+++ app.js
@@ -0,0 +1,10 @@
+// Book Constructor - for creating the book objects
+
+function Book(title, author, isbn){
+    this.title = title
+    this.author = author
+    this.isbn = isbn
+}
+
+// UI Constructor - for methods regarding adding books to list, deleting books, showing alerts, etc
+
