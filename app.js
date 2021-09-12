d040c80195e98eb432c1477f515d17f21b9a65cf
Arslan <ali.arslan319@gmail.com>
2021-09-12 12:02:49 -0400

    create event listener on book form element foor input values

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ function Book(title, author, isbn){
 
 // UI Constructor - for methods regarding adding books to list, deleting books, showing alerts, etc
 
+function UI(){}
+
+// Event listeners 
+
+// grabbing book-form id and listening for a submit event
+document.getElementById('book-form').addEventListener('submit', function(event){
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const isbn = document.getElementById('isbn').value
+    event.preventDefault()
+});
